4dec1465420edb43f388b77fd843e7f909dca88e
elzup <guild0105@gmail.com>
2019-01-05 12:40:54 +0900

    Add reducer

--- src/containers/SpeechArea/reducer.ts
+++ src/containers/SpeechArea/reducer.ts
@@ -3,12 +3,21 @@ import { reducerWithInitialState } from 'typescript-fsa-reducers'
 import { ResultRecord } from '../../types'
 import { actions } from './actions'
 
+type ConfidenceLib = {
+  [key: string]: {
+    sum: number
+    count: number
+    average: number
+  }
+}
+
 // 1分で何文字喋ったか
 export interface State {
   records: ResultRecord[]
   startTime: number | null
   endTime: number | null
   talkPointsByMin: { [key: string]: number }
+  confidencePointsByMin: ConfidenceLib
   // graphData: Array<{ timestamp: number; talkPoint: number }>;
 }
 
@@ -17,6 +26,7 @@ const initialState: State = {
   startTime: null,
   endTime: null,
   talkPointsByMin: {} as { [key: string]: number },
+  confidencePointsByMin: {} as ConfidenceLib,
   // graphData: []
 }
 
@@ -26,12 +36,30 @@ export const reducer = reducerWithInitialState(initialState).case(
     const now = Date.now()
     const min = moment(now).format('YYYY-MM-DDTHH:mm')
     const point = record.text.length
+
+    const confidenceNow = state.confidencePointsByMin[min] || {
+      sum: 0,
+      count: 0,
+      average: 0,
+    }
+    const sum = confidenceNow.sum + record.confidence
+    const count = confidenceNow.count + 1
+    const confidenceNew = {
+      sum,
+      count,
+      average: sum / count,
+    }
+
     return {
       ...state,
       startTime: state.startTime || now,
       talkPointsByMin: {
         ...state.talkPointsByMin,
         [min]: (state.talkPointsByMin[min] || 0) + point,
+      },
+      confidencePointsByMin: {
+        ...state.confidencePointsByMin,
+        [min]: confidenceNew,
       },
       endTime: now,
       records: [...state.records, record],
