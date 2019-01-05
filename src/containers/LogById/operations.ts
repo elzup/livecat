2077eff28f812147f810279525e62484af603cd8
elzup <guild0105@gmail.com>
2019-01-05 14:14:18 +0900

    test: calc log points

--- src/containers/LogById/operations.ts
+++ src/containers/LogById/operations.ts
@@ -1,26 +1,26 @@
 import moment from 'moment'
-import { ThunkAction } from '../../types'
+import { Log, ThunkAction } from '../../types'
 import { updateLog } from './actions'
 import { getLogOrCreateBy } from './selectors'
-
-interface IWindow extends Window {
-  webkitSpeechRecognition: any
-}
 
 export const saveLog = (text: string, confidence: number): ThunkAction => {
   return async (dispatch, getState) => {
     const now = Date.now()
     const min = moment(now).format('YYYY-MM-DDTHH:mm')
     const log = getLogOrCreateBy(getState(), min)
-    const count = log.count + 1
-    const confidentSum = log.confidentSum + confidence
-    const newLog = {
-      ...log,
-      count,
-      confidentSum,
-      confidentAverage: confidentSum / count,
-      point: log.point + text.length,
-    }
-    await dispatch(updateLog([newLog]))
+
+    await dispatch(updateLog([calcLog(log, text.length, confidence)]))
+  }
+}
+
+export const calcLog = (log: Log, point: number, confident: number): Log => {
+  const count = log.count + 1
+  const confidentSum = log.confidentSum + confident
+  return {
+    ...log,
+    count,
+    confidentSum,
+    confidentAverage: confidentSum / count,
+    point: log.point + point,
   }
 }
